1a668f8858f5c1960069ee85e414c3ca926c81fe
Abir <abiirr10@gmail.com>
2025-01-20 16:51:46 +0600

    🐛 FIX: Env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -9,7 +10,8 @@ import { BookModule } from './book/book.module';
   imports: [
     BookModule,
     AuthorModule,
-    MongooseModule.forRoot('mongodb://localhost/maac'),
+    ConfigModule.forRoot({}),
+    MongooseModule.forRoot(`${process.env.DB_URL}`),
   ],
   controllers: [AppController],
   providers: [AppService],
